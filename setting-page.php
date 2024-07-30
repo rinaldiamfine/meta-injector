a64e19d32705ec6d65332527dad6aadc05332308
Rinaldi <rinaldiamfine@gmail.com>
2024-07-30 20:03:29 +0700

    adding the meta inject function for twitter and open graph

--- setting-page.php
+++ setting-page.php
@@ -1,16 +1,62 @@
 <?php
 
 class MetaInjectorSettingPage {
+    // General meta tags
     public $description = '';
     public $keywords = '';
     public $author = '';
     public $robots = '';
 
+    // Open graph meta tags
+    public $ogTitle = '';
+    public $ogDescription = '';
+    public $ogImage = '';
+    public $ogUrl = '';
+    public $ogType = '';
+    public $ogSiteName = '';
+    public $ogLocale = '';
+
+    // Twitter meta tags
+    public $tCard = '';
+    public $tSite = '';
+    public $tTitle = '';
+    public $tDescription = '';
+    public $tImage = '';
+
     public function __construct() {
+        // General meta tags
         $this->description = get_option( 'meta_injector_description', 'This is a custom meta description.' );
         $this->keywords = get_option( 'meta_injector_keywords', 'This is a custom meta description.' );
-        $this->author = get_option( 'meta_injector_author', 'Author' );
+        $this->author = get_option( 'meta_injector_author', 'Author website' );
         $this->robots = get_option( 'meta_injector_robots', 'index, follow' );
+
+        // Open graph meta tags
+        $this->ogTitle = get_option( 'meta_injector_robots', 'index, follow' );
+        $this->ogDescription = get_option( 'meta_injector_robots', 'index, follow' );
+        $this->ogImage = get_option( 'meta_injector_robots', 'index, follow' );
+        $this->ogUrl = get_option( 'meta_injector_robots', 'index, follow' );
+        $this->ogType = get_option( 'meta_injector_robots', 'index, follow' );
+        $this->ogSiteName = get_option( 'meta_injector_robots', 'index, follow' );
+        $this->ogLocale = get_option( 'meta_injector_robots', 'index, follow' );
+        // <meta property="og:title" content="The Rock">
+        // <meta property="og:type" content="video.movie">
+        // <meta property="og:url" content="http://www.imdb.com/title/tt0117500/">
+        // <meta property="og:image" content="http://ia.media-imdb.com/images/rock.jpg">
+        // <meta property="og:description" content="A description of the movie The Rock.">
+        // <meta property="og:site_name" content="IMDb">
+        // <meta property="og:locale" content="en_US">
+
+        // Twitter meta tags
+        $this->tCard = get_option( 'meta_injector_robots', 'index, follow' );
+        $this->tSite = get_option( 'meta_injector_robots', 'index, follow' );
+        $this->tTitle = get_option( 'meta_injector_robots', 'index, follow' );
+        $this->tDescription = get_option( 'meta_injector_robots', 'index, follow' );
+        $this->tImage = get_option( 'meta_injector_robots', 'index, follow' );
+        // <meta name="twitter:card" content="summary_large_image">
+        // <meta name="twitter:site" content="@publisher_handle">
+        // <meta name="twitter:title" content="Page Title">
+        // <meta name="twitter:description" content="Page description less than 200 characters">
+        // <meta name="twitter:image" content="http://www.example.com/image.jpg">
     }
 
     public function setup_hooks() {
@@ -20,10 +66,27 @@ class MetaInjectorSettingPage {
     }
 
     public function inject_tags() {
+        // General meta tags
         echo '<meta name="description" content="' . esc_attr( $this->description ) . '">';
         echo '<meta name="keywords" content="' . esc_attr( $this->keywords ) . '">';
         echo '<meta name="author" content="' . esc_attr( $this->author ) . '">';
         echo '<meta name="robots" content="' . esc_attr( $this->robots ) . '">';
+
+        // Open graph meta tags
+        echo '<meta property="og:title" content="' . esc_attr( $this->ogTitle ) . '">';
+        echo '<meta property="og:type" content="' . esc_attr( $this->ogType ) . '">';
+        echo '<meta property="og:url" content="' . esc_attr( $this->ogUrl ) . '">';
+        echo '<meta property="og:image" content="' . esc_attr( $this->ogImage ) . '">';
+        echo '<meta property="og:description" content="' . esc_attr( $this->ogDescription ) . '">';
+        echo '<meta property="og:site_name" content="' . esc_attr( $this->ogSiteName ) . '">';
+        echo '<meta property="og:locale" content="' . esc_attr( $this->ogLocale ) . '">';
+        
+        // Twitter meta tags
+        echo '<meta name="twitter:card" content="' . esc_attr( $this->tCard ) . '">';
+        echo '<meta name="twitter:site" content="' . esc_attr( $this->tSite ) . '">';
+        echo '<meta name="twitter:title" content="' . esc_attr( $this->tTitle ) . '">';
+        echo '<meta name="twitter:description" content="' . esc_attr( $this->tDescription ) . '">';
+        echo '<meta name="twitter:image" content="' . esc_attr( $this->tImage ) . '">';
     }
 
     public function build_menu() {
